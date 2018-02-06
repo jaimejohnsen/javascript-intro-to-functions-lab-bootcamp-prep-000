3a41f3b59cc39ecd9df9eed4de29375e3851e1f7
Jaime Johnsen <drivetraindiva@gmail.com>
2018-02-06 02:06:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function shout(string) {
   return string.toUpperCase();
 }
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  return console.log(shout(string));
+}
+function logWhisper(string) {
+  return console.log(whisper(string));
+}
+function sayHiToGrandma(string){
+
+if (shout(string) === string) {
+  console.log("YES INDEED!")
+} else if (whisper(string) === string);{
+  console.log(whisper(string); 
+} else (mixedCase.toUpperCase() === mixedCase);{console.log("I love you,Grandma.") // false
+
+}
